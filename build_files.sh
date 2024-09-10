09bc2fe65166e2ff1aaef2719edcfe0291598cf8
nikhil <nikhilgusain9@gmail.com>
2024-09-11 00:38:38 +0530

    changes for vercel

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,5 @@
 echo "BUILD START"
-python3.10 -m pip install -r requirements.txt
-python3.10 manage.py collectstatic --noinput --clear
+.\venv\Scripts\activate 
+python3.9 -m pip install -r requirements.txt
+py manage.py collectstatic --noinput --clear
 echo "BUILD END"
